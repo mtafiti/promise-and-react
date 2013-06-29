a14a39ca4c245cb543cab3b7695d1ce4d8c043e7
Kennedy K <kkambona@vub.ac.be>
2013-06-29 02:21:57 +0200

    bug fixes

--- routes/ajax.js
+++ routes/ajax.js
@@ -1,25 +1,24 @@
-
 //var utilities = require('utilities');
 
-exports.initialize = function(mynow){
+exports.initialize = function (mynow) {
     console.log("initializing mynow in ajax...");
-       //myajaxnow = mynow;
+    //myajaxnow = mynow;
     //return myajaxnow;
 };
 
-exports.ajax_post = function (request,response,myajaxnow){
+exports.ajax_post = function (request, response, myajaxnow) {
 
 //clean get request
     var pathname = request.url;
     var content = '';
-    console.log("post was called with params "+ request.params);
+    console.log("post was called with params " + request.params);
 
 
-    if (request.params["savesession"]){
+    if (request.params["savesession"]) {
         //add data to rooms object
         myajaxnow.saveData(postparams.data);
-            content = JSON.stringify({success: true, msg: 'Session saved successfully'});
-    } else if (request.params["loadsession"]){
+        content = JSON.stringify({success: true, msg: 'Session saved successfully'});
+    } else if (request.params["loadsession"]) {
         //get room session
         var data = myajaxnow.getData(postparams.rname);
         content = JSON.stringify({data: data, success: true});
@@ -28,20 +27,27 @@ exports.ajax_post = function (request,response,myajaxnow){
     response.json({"rooms": content}, 200);
 };
 
-exports.ajax_get = function(request, response, myajaxnow){
+/**
+ * Soon to be deprecated
+ * @param request
+ * @param response
+ * @param myajaxnow
+ */
+exports.ajax_get = function (request, response, myajaxnow) {
     //clean get request
     var pathname = request.url;
     var content = '';
 
     //GET Ajax request
-    if (pathname.indexOf('?') !== -1){
-        pathname = pathname.substring(pathname.indexOf('?')+1,pathname.length);
+    if (pathname.indexOf('?') !== -1) {
+        pathname = pathname.substring(pathname.indexOf('?') + 1, pathname.length);
     }
-    console.log("Request handler 'ajax' was called. pathname:"+pathname);
+    console.log("Request handler 'ajax' was called. pathname:" + pathname);
 
-    if (request.param("request") === "loadrooms"){
+    if (request.param("request") === "loadrooms") {
         var rms = myajaxnow.getAllRoomnames();
         content = JSON.stringify({success: true, rooms: rms});
     }
+    //respond
     response.json({"rooms": content}, 200);
 };
